fc3349f337a658de17f312c1ca8037fb714824e5
Ryan Burrows <rhburrows@gmail.com>
2010-11-03 19:04:16 -0700

    Identify as iphone

--- lib/capybara/driver/iphone_driver.rb
+++ lib/capybara/driver/iphone_driver.rb
@@ -2,5 +2,12 @@ require 'capybara/driver/rack_test_driver'
 
 module Capybara::Driver
   class Iphone < RackTest
+    USER_AGENT = "Mozilla/5.0 (iPhone; U; CPU like Mac OS X; en)" +
+      " AppleWebKit/420+ (KHTML, like Gecko) Version/3.0" +
+      " Mobile/1A543a Safari/419.3"
+
+    def env
+      super.merge("HTTP_USER_AGENT" => USER_AGENT)
+    end
   end
 end
