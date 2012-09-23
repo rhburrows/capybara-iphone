cc31c8f6285c2d69937e666df6e6d8cf85c2fab0
dblock <dblock@dblock.org>
2012-09-23 12:15:56 -0400

    Fix: updated to Capybara/Sinatra newest versions.

--- lib/capybara/driver/iphone_driver.rb
+++ lib/capybara/driver/iphone_driver.rb
@@ -1,7 +1,7 @@
 require 'capybara/rack_test/driver'
 
 module Capybara::Driver
-  class Iphone < RackTest
+  class Iphone < Capybara::RackTest::Driver
     USER_AGENT = "Mozilla/5.0 (iPhone; U; CPU like Mac OS X; en)" +
       " AppleWebKit/420+ (KHTML, like Gecko) Version/3.0" +
       " Mobile/1A543a Safari/419.3"
